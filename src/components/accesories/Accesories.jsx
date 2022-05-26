90a34f0810406cd943bd080623f53c5d6e68a54a
Rijad <rijad.comor@fsk.unsa.ba>
2022-05-26 02:51:59 +0200

    responsive

--- src/components/accesories/Accesories.jsx
+++ src/components/accesories/Accesories.jsx
@@ -3,6 +3,7 @@ import './accesories.css'
 import data from './data'
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+import { Pagination } from 'swiper';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
@@ -10,13 +11,13 @@ import 'swiper/css/pagination';
 const Accesories = () => {
   return (
     <section id='accesories'>
-      <h5>Proxy</h5>
+      <h5>New</h5>
       <h2>Accesories</h2>
       <Swiper className='acc'
-      //modules={[Pagination]}
-      //spaceBetween={40}
-      //slidesPerView={1}
-      //pagination={{ clickable: true }}
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{ clickable: true }}
       >
         <div className='accesories__container'>
         {data.map((data,indexData)=>{
@@ -26,12 +27,14 @@ const Accesories = () => {
                 <div className="accesories__item-img">
                   <img src={data.image} alt={data.name} />
                 </div>
-                <h3>{data.name}</h3>
-                <h4>{data.group}</h4>
-                <h5>{data.gender}</h5> <h5>{data.price}</h5>
-                <br />
-                <a href="https://google.ba" target='_blank' className='btn btn-gap'>Details</a>
-                <a href="https://google.ba" target='_blank' className='btn btn-primary'>Add to cart</a>
+                <div className='accesories__item-details'>
+                  <h3>{data.name}</h3>
+                  <h4>{data.group}</h4>
+                  <h5>{data.gender}</h5> <h5>{data.price}</h5>
+                  <br />
+                  <a href="https://google.ba" target='_blank' className='btn btn-gap'>Details</a>
+                  <a href="https://google.ba" target='_blank' className='btn btn-primary'>Add to cart</a>
+                </div>
               </SwiperSlide>
             )})}
         </div>
